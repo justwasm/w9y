0643b87e720a464151bbed1ff891c9d483fc70e1
exe.dev <exe.dev@gmail.com>
2026-06-17 03:28:54 +0000

    tar xzc

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sL https://github.com/justwasm/go/releases/download/go1.27.0-wanix.6/go1.27.0-wanix.6.linux-amd64.min.tar.gz | tar -xvzC /
+curl -sL https://github.com/justwasm/go/releases/download/go1.27.0-wanix.6/go1.27.0-wanix.6.linux-amd64.min.tar.gz | tar -xzC /
 
 export PATH=/go/bin:$PATH
 
