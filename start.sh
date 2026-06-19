39fc73f4a9e6db10ba7f999fdc7c6970ac22757e
Broken Pipe <navigaid@gmail.com>
2026-06-19 13:37:42 +0800

    go4js

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -sL https://github.com/justwasm/go/releases/download/go1.27.0-wanix.6/go1.27.0-wanix.6.linux-amd64.min.tar.gz | tar -xzC /
+TAG=go1.27.0-go4js.1
+
+curl -sL https://github.com/justwasm/go/releases/download/${TAG}/${TAG}.linux-amd64.min.tar.gz | tar -xzC /
 
 export PATH=/go/bin:/app/bin:$PATH
 
