2b10691ffe46d380e062746412f2ac8a8de40d83
Broken Pipe <navigaid@gmail.com>
2026-07-01 04:00:51 +0800

    add start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 TAG=go1.27.0-go4js.1
 
-curl -sL https://github.com/justwasm/go/releases/download/${TAG}/${TAG}.linux-amd64.min.tar.gz | tar -xzC /
+[[ -f /go/bin/go ]] || {
+  curl -sL https://github.com/justwasm/go/releases/download/${TAG}/${TAG}.linux-amd64.min.tar.gz | tar -xzC /
+}
 
 export PATH=/go/bin:/app/bin:$PATH
 
